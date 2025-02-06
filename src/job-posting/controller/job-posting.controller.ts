75500dda0c32ce44816b1665e11b2b914670c830
ta1355 <eorbs255@naver.com>
2025-02-07 08:42:41 +0900

    구인구직 컨트롤러 cru 추가

--- src/job-posting/controller/job-posting.controller.ts
+++ src/job-posting/controller/job-posting.controller.ts
@@ -1,10 +1,14 @@
 import {
   Body,
   Controller,
+  Get,
   HttpException,
   HttpStatus,
   InternalServerErrorException,
+  Param,
+  Patch,
   Post,
+  Query,
   Req,
   UseGuards,
 } from '@nestjs/common';
@@ -12,6 +16,7 @@ import { JobPostingService } from '../service/job-posting.service';
 import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
 import { CreateJobPostingDto } from '../dto/create-job-posting.dto';
 import { User } from 'src/auth/entity/user.entity';
+import { UpdateJobPostingDto } from '../dto/update-job-posting.dto';
 
 interface RequestWithUser extends Request {
   user: User;
@@ -28,6 +33,37 @@ export class JobPostingController {
       return await this.jobPostingService.createJobPosting(dto, req.user);
     } catch (error: unknown) {
       throw this.handleError(error, 'Failed to create job-posting');
+    }
+  }
+
+  @Get()
+  async findAll(@Query('page') page: number, @Query('limit') limit: number) {
+    try {
+      return await this.jobPostingService.getAllJobPostings(page, limit);
+    } catch (error) {
+      throw this.handleError(error, 'Failed to findAll job-posting');
+    }
+  }
+
+  @Get(':id')
+  async findOne(@Param('id') id: string) {
+    try {
+      return await this.jobPostingService.getJobPostingById(+id);
+    } catch (error) {
+      throw this.handleError(error, 'Failed to find one job-posting');
+    }
+  }
+
+  @Patch(':id')
+  async update(
+    @Param('id') id: string,
+    @Body() dto: UpdateJobPostingDto,
+    @Req() req: RequestWithUser,
+  ) {
+    try {
+      return await this.jobPostingService.updateJobPosting(+id, dto, req.user);
+    } catch (error) {
+      throw this.handleError(error, 'Faild to update job-posting');
     }
   }
 
